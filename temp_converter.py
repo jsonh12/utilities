985c32137c043db5b29911a2b9e21f7660fe37fb
jsonh12 <jsonh12@icloud.com>
2025-02-23 20:45:36 +0100

    Fix Indents

--- temp_converter.py
+++ temp_converter.py
@@ -1,13 +1,11 @@
-#def nazwaFunkcji(argument1, argument2, …)
+# def nazwaFunkcji(argument1, argument2, …)
 #	operacje i działania
 #	operacje i działania
 #	print(rezultaty)
 
-
+# Zadania:
 # Napisać funkcje która przelicza stopnie Celsjusza na farenheit
-
 # Napisać funkcje która przelicza stopnie farenheit na Celsjusza
-
 # Napisać funkcje która przelicza stopnie Celsjusza na farenheit lub odwrotnie w zależności od argumentu
 
 
@@ -32,7 +30,6 @@ def twoWayConverter(TempValue=0, TempType='C'):
 		CTemp = (TempValue - 32)/(9/5)
 		print(CTemp)
 
-
 # Zamiana C na F lub F na C w jednej funkcji - refactoring do jednego print'a
 def twoWayConverterV2(TempValue=0, TempType='C'):
 	if(TempType == 'C'):
@@ -46,22 +43,18 @@ def twoWayConverterV3(TempValue=0, TempType='KtoC'):
 	# Konwersja z C do F
 	if(TempType == 'CtoF'):
 		Result = 2 * (TempValue - 0.1 * TempValue) + 32
-	if(TempType == 'FtoC'):
+	elif(TempType == 'FtoC'):
 		Result = (TempValue - 32)/(9/5)
 	elif(TempType == 'KtoC'):
 		Result = (TempValue - 273.15)
 	elif(TempType == 'CtoK'):
 		Result = (TempValue + 273.15)
 	elif(TempType == 'KtoF'):
-	        Result = ( 1.8 * (TempValue - 273.15) + 32)
+		Result = ( 1.8 * (TempValue - 273.15) + 32)
 	# elif(Temptype == 'FtoK'):
 	#	Result = ()	
 	else:
 		print("Conversion not supported!")
 	print(Result)
-
-
-
-
 
 
