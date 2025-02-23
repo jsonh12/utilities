066e5e6dc4f314090b8bf4eaf0a17661ebafd1e0
jsonh12 <jsonh12@icloud.com>
2025-02-23 22:01:13 +0100

    Update temp_converter.py

--- temp_converter.py
+++ temp_converter.py
@@ -30,6 +30,7 @@ def twoWayConverter(TempValue=0, TempType='C'):
         CTemp = (TempValue - 32)/(9/5)
         print(CTemp)
 
+
 # Zamiana C na F lub F na C w jednej funkcji - refactoring do jednego print'a
 def twoWayConverterV2(TempValue=0, TempType='C'):
     if (TempType == 'C'):
@@ -38,22 +39,24 @@ def twoWayConverterV2(TempValue=0, TempType='C'):
         Result = (TempValue - 32)/(9/5)
     print(Result)
 
+
 def twoWayConverterV3(TempValue=0, TempType='KtoC'):
     # Konwersja z C do F
     if (TempType == 'CtoF'):
         Result = 2 * (TempValue - 0.1 * TempValue) + 32
     elif (TempType == 'FtoC'):
-        Result = (TempValue - 32)/(9/5)
+        Result = (TempValue - 32) / (9/5)
     elif (TempType == 'KtoC'):
         Result = (TempValue - 273.15)
     elif (TempType == 'CtoK'):
         Result = (TempValue + 273.15)
     elif (TempType == 'KtoF'):
-        Result = 1.8 * (TempValue - 273.15) + 32
+        Result = (1.8 * (TempValue - 273.15) + 32)
     elif (TempType == 'FtoK'):
-        Result = (TempValue - 32)/ 1.8 + 273.15
+        Result = ((TempValue - 32) / 1.8 + 273.15)
     else:
         print("Conversion not supported!")
+    
     print(Result)
 
 
