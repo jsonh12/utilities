67e1b6dcd73d7f97281f3529e32626dcdf2e1e1f
Juliusz Cezar1212 <jsonh12@icloud.com>
2025-03-08 12:16:31 +0100

    fixes

--- server.py
+++ server.py
@@ -8,15 +8,17 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
-@app.route('/temp/')
+@app.route('/temp')
 def temp():
   result = twoWayConverterV2(21, 'C')
   print(result)
+  return result
 
 @app.route('/measure')
 def measure():
   result = AreaMeasurer('Triangle', 8, 9,'CM')
   print(result)
+  return result
 
 if __name__ == '__main__':
   app.run(debug=True)
