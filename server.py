491a56d7215cea242ea2fbbfb4bbd19c2c1d1b74
Juliusz Cezar1212 <jsonh12@icloud.com>
2025-03-08 12:13:51 +0100

    fixes

--- server.py
+++ server.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
+from file.py import twoWayConverterV2, AreaMeasurer
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/my-link/')
-def my_link():
-  print('I got clicked!')
+@app.route('/temp/')
+def temp():
+  result = twoWayConverterV2(21, 'C')
+  print(result)
 
-  return 'Click.'
+@app.route('/measure')
+def measure():
+  result = AreaMeasurer('Triangle', 8, 9,'CM')
+  print(result)
 
 if __name__ == '__main__':
   app.run(debug=True)
